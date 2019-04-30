0713fb545216e526ffea80a064ecd5c99ad518cf
jpca999 <jpca999@gmail.com>
2019-04-29 18:07:34 -0700

    15-7 orking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,9 @@ export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
-  onSubmit(form: HTMLFormElement){
-    console.log('13- on Submit called n  f =', form);
+  // onSubmit(form: HTMLFormElement){
+  onSubmit(form: NgForm){
+    console.log('15- on Submit called n  form =', form);
 
     
 
